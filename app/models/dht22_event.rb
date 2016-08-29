06b6907ef5ad95f5b08a55d0f70cf0014f95f7ab
Jack Willis <jack@attac.us>
2016-08-29 17:14:44 -0500

    Create DHT22Event model

---
+++ app/models/dht22_event.rb
@@ -0,0 +1,3 @@
+class DHT22Event < ApplicationRecord
+  self.table_name = :dht22
+end
---
+++ test/models/dht22_event_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Dht22EventTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
